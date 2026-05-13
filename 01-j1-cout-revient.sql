226978131ef322eb24d0fcf3b6801f2fcb0c97fb
MISSAMOU-Mesmer <mesmermissamou2@gmail.com>
2026-05-13 23:10:06 +0200

    Create 01-j1-cout-revient.sql

---
+++ 01-j1-cout-revient.sql
@@ -0,0 +1,6 @@
+--- J1 PAIN BOSS PNR - Coût de Revient 20 pains
+SELECT
+     'DBA Pain Boss PNR' AS Message,
+      20 * 250 AS ChiffreAffaire,
+      1790 AS CoutProduction20Pains,
+      20 * 250 - 1790 AS BeneficeNet;
